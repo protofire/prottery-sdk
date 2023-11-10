7070943b54def8c6302d0394285e9c0c993f4701
Luigi <luigibyte@gmail.com>
2023-11-10 15:15:47 -0300

    wip

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const config: Config = new Map([
   [
     11155111, 
     { 
-      PROTTERY: "0x94aF31F385a8253Dd4c5B74E56e6c21bAE242826"
+      PROTTERY: "0x94aF31F385a8253Dd4c5B74E56e6c21bAE242826",
     }
   ]
 ]);
